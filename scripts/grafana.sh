fdb36b03f8daef8a303d4131de4d169765ba0c8d
diba1013 <diba1013@web.de>
2025-11-15 15:42:20 +0100

    feat(victoria-metrics): allow to configure a static secret for the grafana dashboard
    Signed-off-by: diba1013 <diba1013@web.de>

--- scripts/grafana.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Retrieve the grafana password from the kubernetes secret
-
-GRAFANA_PASSWORD=$(kubectl get secret victoria-metrics-grafana -n victoria-metrics -o jsonpath="{.data.admin-password}" | base64 --decode)
-
-echo "$GRAFANA_PASSWORD"
